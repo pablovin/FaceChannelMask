2eeb32e2c291cbc75a4ec86d4bee0aac1faf3cb2
Pablo <pablo.alvesdebarros@iit.it>
2020-05-27 11:27:17 +0200

    Adding webcam demo

---
+++ liveRecognitionUtils/modelDictionary.py
@@ -0,0 +1,7 @@
+class DimensionalMask:
+    modelname = "Arousal and Valence Trained on AffectNet"
+    modelDirectory = "trainedModels/FaceChannelMask_250k_scratch.h5"
+    modelType = "Dimensional"
+    classsesOrder = ["Arousal", "Valence"]
+    classesColor = [(0, 255, 0), (255, 0, 0)]
+
